d4dfe9d55f095db3245287cc74eb54338b2b87a4
viniciuspc <pasqualini9@gmail.com>
2025-12-27 12:56:12 +0000

    Add multiple messages capabilities

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,8 +13,10 @@ parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 def main():
-    generated_content = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+    generated_content = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     usage_metadata = generated_content.usage_metadata
     if usage_metadata:
         print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
