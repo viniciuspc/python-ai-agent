bb50aeb5c0cdfd8c8d68bc7afb1016c4587fad6c
viniciuspc <pasqualini9@gmail.com>
2025-12-27 13:01:02 +0000

    Add verbose flag

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -19,8 +20,10 @@ def main():
     generated_content = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     usage_metadata = generated_content.usage_metadata
     if usage_metadata:
-        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {usage_metadata.candidates_token_count}")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {usage_metadata.candidates_token_count}")
         print(generated_content.text)
     else:
         raise RuntimeError("Failed to connect to Gemini API")
