a907faa07550dfcd779d56c477cf6a729963619c
viniciuspc <pasqualini9@gmail.com>
2025-12-29 12:43:16 +0000

    Add system prompt and start declaring tools for agent use

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
     try:
@@ -24,3 +25,17 @@ def get_files_info(working_directory, directory="."):
         return "\n".join(dir_items_data)
     except Exception as e:
         return f"Error: {e}"
+    
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in a specified directory relative to the working directory, providing file size and directory status",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from functions.call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -17,14 +19,27 @@ args = parser.parse_args()
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 def main():
-    generated_content = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    generated_content = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+            ),
+        )
     usage_metadata = generated_content.usage_metadata
     if usage_metadata:
         if args.verbose:
             print(f"User prompt: {args.user_prompt}")
             print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
             print(f"Response tokens: {usage_metadata.candidates_token_count}")
-        print(generated_content.text)
+        
+        function_calls = generated_content.function_calls
+        
+        if function_calls:
+            for function_call in function_calls:
+                print(f"Calling function: {function_call.name}({function_call.args})")    
+        else:
+            print(generated_content.text)
     else:
         raise RuntimeError("Failed to connect to Gemini API")
 
